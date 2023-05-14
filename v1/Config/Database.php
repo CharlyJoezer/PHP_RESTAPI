3c9a9011397ec4863ea180d390a45eb30e7509f5
CharlyJoezer <charlyjoezer30@gmail.com>
2023-05-14 23:10:04 +0800

    [EDIT] UBAH KONFIGURASI DATABASE

--- v1/Config/Database.php
+++ v1/Config/Database.php
@@ -1,14 +1,19 @@
 <?php
 
+namespace Backend\Config;
+use PDO;
+use Exception;
+
 class Database {
      private $db_host = 'localhost';
      private $db_name = 'php_restapi';
      private $db_user = 'root';
      private $db_pass = '';
+     private $db_port = '3308';
 
      public function __construct(){
           try{
-               $conn = new PDO("mysql:host=$this->db_host;dbname=$this->db_name", $this->db_user, $this->db_pass);
+               $conn = new PDO("mysql:host=$this->db_host;port=$this->db_port;dbname=$this->db_name", $this->db_user. $this->db_pass);
                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
           }catch(Exception $e){
                echo $e->getMessage();
