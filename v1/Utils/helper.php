8cf5f06bc5e89b92fa65eb76e5fe13c73b44a4d9
CharlyJoezer <charlyjoezer30@gmail.com>
2023-05-17 19:44:26 +0800

    Menambahkan Fungsi Helper untuk bind values insert SQL

--- v1/Utils/helper.php
+++ v1/Utils/helper.php
@@ -7,4 +7,11 @@ class Helper {
      public static function Controller($class, $method, $data = null){
           return $class->$method();
      }
+
+     public static function arrayKeyToBind($data){
+          return (string)implode(', ',  array_map(function($key){
+                                              return ":".$key;
+                                        }, array_keys($data))
+          );
+     }
 }
