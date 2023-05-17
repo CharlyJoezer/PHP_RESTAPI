c17641c2d05e757376fa794a0c1a367c1f221e01
CharlyJoezer <charlyjoezer30@gmail.com>
2023-05-17 12:39:19 +0800

    [EDIT] Menambahkan script untuk query di class database

--- v1/Config/Database.php
+++ v1/Config/Database.php
@@ -10,14 +10,56 @@ class Database {
      private $db_user = 'root';
      private $db_pass = '';
      private $db_port = '3308';
+     private $db;
+     private $stmt;
 
      public function __construct(){
           try{
                $conn = new PDO("mysql:host=$this->db_host;port=$this->db_port;dbname=$this->db_name", $this->db_user. $this->db_pass);
                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+               $this->db = $conn;
           }catch(Exception $e){
                echo $e->getMessage();
           }
      }
+
+     public function query($query){
+          $this->stmt = $this->db->prepare($query);
+      }
+      public function execute(){
+          $this->stmt->execute();
+      }
+  
+  
+      public function getAll()
+      {
+          $this->execute();
+          return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+      }
+      public function single(){
+          $this->execute();
+          return $this->stmt->fetch(PDO::FETCH_ASSOC);
+      }
+      
+      public function bind($param, $value, $type = null)
+      {
+          if( is_null($type)){
+              switch(true) {
+                  case is_int($value) :
+                      $type = PDO::PARAM_INT;
+                      break;
+                  case is_bool($value) :
+                      $type = PDO::PARAM_BOOL;
+                      break;
+                  case is_null($value) :
+                      $type = PDO::PARAM_NULL;
+                      break;
+                  default :
+                      $type = PDO::PARAM_STR;
+              }
+          }
+  
+          $this->stmt->bindValue($param, $value, $type);
+      }
      
 }
