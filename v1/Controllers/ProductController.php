53c40aeffb037fe5c3099f442e0402207a3a3632
CharlyJoezer <charlyjoezer30@gmail.com>
2023-05-19 12:03:28 +0800

    Menambahkan beberapa method di Controller Product

--- v1/Controllers/ProductController.php
+++ v1/Controllers/ProductController.php
@@ -5,5 +5,20 @@ namespace Backend\Controllers;
 use Backend\Models\Product;
 
 class ProductController {
-     
+     public function getAllDataProduct(){
+          $prd = new Product;
+          $getData = $prd->select();
+          var_dump($getData);
+          die();
+     }
+
+     public function insert(){
+          $request = [
+               'name' => $_POST['name'],
+               'price' => $_POST['price']
+          ];
+
+          $prd = new Product;
+          echo $prd->insert($request);
+     }
 }
