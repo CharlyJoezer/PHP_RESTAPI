8b37ff4379e3aa710e8b32e3705ae70a2e1056f8
CharlyJoezer <charlyjoezer30@gmail.com>
2023-05-19 12:04:12 +0800

    Menambahkan Method untuk query orm insert

--- v1/Config/ORM.php
+++ v1/Config/ORM.php
@@ -3,6 +3,7 @@
 namespace Backend\Config;
 
 use Backend\Config\Database;
+use Backend\Utils\Helper;
 
 class ORM extends Database {
      protected $db;
@@ -16,5 +17,17 @@ class ORM extends Database {
           $this->connectDB();
           $this->db->query("SELECT * FROM ". $this->table);
           return $this->db->getAll();
+     }
+
+     public function insert($data){
+          $this->connectDB();
+          $query = "INSERT INTO $this->table (". (string)implode(', ', array_keys($data)) . ") VALUES (".Helper::arrayKeyToBind($data).")";
+          $this->db->query($query);
+          for($i = 0; $i < count($data); $i++){
+               $key = (string)array_keys($data)[$i];
+               $this->db->bind($key, $data[$key]);
+          }
+          $this->db->execute();
+          return true;
      }
 }
