89e885acf9c6a27f4d4a598486206f48a8469b0c
CharlyJoezer <charlyjoezer30@gmail.com>
2023-05-19 11:35:52 -0700

    Membuat method helper untuk mengembalikan response dari Request

--- v1/Utils/helper.php
+++ v1/Utils/helper.php
@@ -14,4 +14,11 @@ class Helper {
                                         }, array_keys($data))
           );
      }
+
+     public static function response(Int $code, Array $message = []){
+          http_response_code($code);
+          header('Content-Type: application/json');
+          echo json_encode($message);
+          exit();
+     }
 }
