56542b285d487b2d978f696d6ab44bfec6c3e967
CharlyJoezer <charlyjoezer30@gmail.com>
2023-05-20 21:33:56 +0800

    Membuat validator untuk validasi request

---
+++ v1/Utils/validator.php
@@ -0,0 +1,49 @@
+<?php
+namespace Backend\Utils;
+use Backend\Utils\Helper;
+
+class Validator {
+     public static function validate(Array $data){
+          (Array) $error = [];
+          foreach($data as $key => $val){
+               $obj = new self();
+               $invalid = $obj->helperValidate($val, $key);
+               if(isset($invalid)){
+                    array_push($error, [$key => $invalid]);
+               }
+          }
+
+          if(count($error) > 0){
+               return Helper::response(400, [
+                    'errors' => $error,
+                    'message' => 'Input is not Validated',
+                    'code' => 400
+               ]);
+          }else{
+               return true;
+          }
+
+
+     }
+     protected function helperValidate($cond, $key){
+          $httpMethod = $_SERVER['REQUEST_METHOD'];
+          $data = [];
+          if($httpMethod == 'GET'){
+               $data = $_GET;
+          }elseif($httpMethod == 'POST'){
+               $data = $_POST;
+          }
+          foreach($cond as $val){
+               if('required' == $val){
+                    if( empty($data[(String) $key]) ){
+                         return $key . ' is required';
+                    }
+               }
+               if('numeric' == $val){
+                    if( !is_numeric($data[(String) $key]) ){
+                         return $key . ' must be a number';
+                    }
+               }
+          }
+     }
+}
