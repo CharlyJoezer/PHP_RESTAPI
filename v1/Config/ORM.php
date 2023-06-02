a3d7a83942c959fc05c7babf31ce2b99569e2855
CharlyJoezer <charlyjoezer30@gmail.com>
2023-06-02 22:55:44 +0800

    Membuat ORM dengan "Method Chaining"

--- v1/Config/ORM.php
+++ v1/Config/ORM.php
@@ -8,62 +8,34 @@ use Backend\Utils\Helper;
 class ORM extends Database {
      protected $db;
      public $table = NULL;
+     public $where,
+             $join;
 
      private function connectDB(){
           $this->db = new Database;
      }
 
-     public function select(){
-          $this->connectDB();
-          $this->db->query("SELECT * FROM ". $this->table);
-          return $this->db->getAll();
-     }
-
-     public function first($field, $opr, $val){
-          $this->connectDB();
-          $this->db->query("SELECT * FROM $this->table WHERE $field $opr :val");
-          $this->db->bind("val", (Int)$val);
-          return $this->db->getAll();
-     }
-
-     public function insert($data){
-          $this->connectDB();
-          $query = "INSERT INTO $this->table (". (string)implode(', ', array_keys($data)) . ") VALUES (".Helper::arrayKeyToBind($data).")";
-          $this->db->query($query);
-          for($i = 0; $i < count($data); $i++){
-               $key = (string)array_keys($data)[$i];
-               $this->db->bind($key, $data[$key]);
-          }
-          $this->db->execute();
-          return true;
-     }
-
-     public function update(String $where, Array $data){
-          $this->connectDB();
-          unset($data['id']);
-          $strUpdtCol = '';
-          foreach($data as $key => $val){
-               if($key === array_key_last($data)){
-                    $strUpdtCol .= "$key = :$key";
-               }else{
-                    $strUpdtCol .= "$key = :$key,";
+     public function where(Array $cond){
+          if(is_array($cond[0])){
+               $str = '';
+               foreach($cond as $key => $item){
+                    if($key != (count($cond) - 1)){
+                         $str .= $item[0].$item[1].$item[2].',';
+                    }else{
+                         $str .= $item[0].$item[1].$item[2];
+                    }
                }
-          }          
-          $query = "UPDATE $this->table SET $strUpdtCol WHERE $where;";
-          $this->db->query($query);
-          for($i = 0; $i < count($data); $i++){
-               $key = (string)array_keys($data)[$i];
-               $this->db->bind($key, $data[$key]);
+               $this->where = $str;
+          }else{
+               $str = $cond[0].$cond[1].$cond[2];
+               $this->where = $str;
           }
-          $this->db->execute();
-          return true;
+          return $this;
      }
-
-     public function delete($field, $cond ,Int $val){
+     
+     public function get(){
           $this->connectDB();
-          $query = "DELETE FROM product WHERE $field $cond $val";
-          $this->db->query($query);
-          $this->db->execute();
-          return true;
+          $this->db->query("SELECT * FROM $this->table WHERE $this->where");
+          return $this->db->getAll();         
      }
 }
