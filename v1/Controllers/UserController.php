b241a84d574e6df10cd20d26f7762012a7891847
CharlyJoezer <charlyjoezer30@gmail.com>
2023-06-03 20:29:10 +0800

    Menambahkan UserController

---
+++ v1/Controllers/UserController.php
@@ -0,0 +1,11 @@
+<?php
+namespace Backend\Controllers;
+
+use Backend\Models\User;
+use Backend\Utils\Validator;
+use Backend\Utils\Helper;
+use Backend\Utils\Request;
+use Exception;
+class UserController{
+    
+}
