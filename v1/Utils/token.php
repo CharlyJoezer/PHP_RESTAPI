658af1e1dc26246ba9c5a1edf3cb0225e27bfd07
CharlyJoezer <charlyjoezer30@gmail.com>
2023-06-10 11:09:14 +0800

    Membuat Function untuk memvalidasi token yang dikirim

--- v1/Utils/token.php
+++ v1/Utils/token.php
@@ -1,6 +1,31 @@
 <?php
 namespace Backend\Utils;
 
+use Backend\Utils\Helper;
+use Backend\Models\Token as modelToken;
+
 class Token {
-  
+    public static function tokenValidation(){
+        $token = $_SERVER['HTTP_AUTHORIZATION'];
+        if(isset($token)){
+            $tknModel = new modelToken;
+            $getTokenDB = $tknModel->where(['token', '=', $token])->get();
+            if(count($getTokenDB) > 0){
+                return true;
+            }else{
+                return Helper::response(401, [
+                    'code' => 401,
+                    'status' => false,
+                    'message' => 'Your token is not match'
+                ]);
+            }
+            Helper::response(200, $getTokenDB);
+        }else{
+            return Helper::response(401, [
+                'code' => 401,
+                'status' => false,
+                'message' => 'Auth required, your token is not found!'
+            ]);
+        }
+    }
 }
